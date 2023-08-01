700fd4fd38c81b0e773875ddaea1a408b3d04cdd
sxs <xingsheng.sheng@outlook.com>
2023-08-01 23:59:44 +0800

    feat: 3.3.2 annotate the domain as entities

--- src/main/java/org/sxs/tacocloud/domain/Ingredient.java
+++ src/main/java/org/sxs/tacocloud/domain/Ingredient.java
@@ -1,34 +1,31 @@
 package org.sxs.tacocloud.domain;
 
-import lombok.AccessLevel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
+
 
 /**
  * @author sxs
  * @date 2023/7/22
  */
 
-@Table
+@Entity
 @Data
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient implements Persistable<String> {
+@NoArgsConstructor(force = true)
+public class Ingredient {
     @Id
     private String id;
     private String name;
     private Type type;
 
-    @Override
-    public boolean isNew() {
-        return true;
-    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
+
+
 }
--- src/main/java/org/sxs/tacocloud/domain/Taco.java
+++ src/main/java/org/sxs/tacocloud/domain/Taco.java
@@ -1,21 +1,19 @@
 package org.sxs.tacocloud.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author sxs
  * @date 2023/7/22
  */
-@Table
+@Entity
 @Data
 public class Taco implements Serializable {
     public static final long serialVersionUID = 1L;
@@ -25,17 +23,6 @@ public class Taco implements Serializable {
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
-    /**
-     * spring mvc 怎么将表单中的ingredients:x,ingredients:xx,..转换成List<IngredientRef>
-     * 答案：是类型转换器,用到两个StringToCollectionConverter和ObjectToObjectConverter
-     * 1.StringToCollectionConverter：生成一个ArrayList<IngredientRef>,然后用ObjectToObjectConverter处理元素
-     * 2.ObjectToObjectConverter：将单个String x转换为IngredientRef,转换步骤是找到IngredientRef的为参数为String的构造器，然后反射调用，就获得了一个IngredientRef
-     */
-    @NotNull
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients = new ArrayList<>();
 
-    public void addIngredient(Ingredient ingredient) {
-        ingredients.add(new IngredientRef(ingredient.getId()));
-    }
+
 }
